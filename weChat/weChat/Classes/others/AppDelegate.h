c40c9de2309fed7a3af7f8e412b1d8bb06aaf3de
XSUNT45 <xsunt45@Nash.local>
2017-03-10 17:06:05 +0800

    update

--- weChat/weChat/Classes/others/AppDelegate.h
+++ weChat/weChat/Classes/others/AppDelegate.h
@@ -8,43 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-typedef enum {
-    XMPPResultTypeLoginSuccess,//登录成功
-    XMPPResultTypeLoginFailure,//登录失败
-    XMPPResultTypeNetErr,//网络不给力
-    XMPPResultTypeRegisterSuccess,//注册成功
-    XMPPResultTypeRegisterFailure//注册失败
-}XMPPResultType;
-
-// XMPP请求结果的block
-typedef void(^XMPPResultsBlock) (XMPPResultType type);
-
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
-/**
- *  注册标识 YES 注册 / NO 登录
- */
-@property (nonatomic, assign,getter=isRegisterOperation) BOOL  registerOperation;//注册操作
-
-
-
-/**
- *  用户登录
- */
--(void)xmppUserLogin:(XMPPResultsBlock)result;
-
-/**
- *  用户注销
- */
--(void)xmppUserlogout;
-
-/**
- *  用户注册
- */
--(void)xmppUserRegister:(XMPPResultsBlock)resultBlock;
 
 @end
 
---
+++ weChat/weChat/Classes/public/singleton/WCXMPPTool.h
@@ -0,0 +1,51 @@
+//
+//  WCXMPPTool.h
+//  weChat
+//
+//  Created by XSUNT45 on 17/3/10.
+//  Copyright © 2017年 XSUNT45. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "WSSingleton.h"
+
+typedef enum {
+    XMPPResultTypeLoginSuccess,//登录成功
+    XMPPResultTypeLoginFailure,//登录失败
+    XMPPResultTypeNetErr,//网络不给力
+    XMPPResultTypeRegisterSuccess,//注册成功
+    XMPPResultTypeRegisterFailure//注册失败
+}XMPPResultType;
+
+// XMPP请求结果的block
+typedef void(^XMPPResultsBlock) (XMPPResultType type);
+
+
+
+@interface WCXMPPTool : NSObject
+WSSingletonH(WCXMPPTool)
+
+
+/**
+ *  注册标识 YES 注册 / NO 登录
+ */
+@property (nonatomic, assign,getter=isRegisterOperation) BOOL  registerOperation;//注册操作
+
+
+
+/**
+ *  用户登录
+ */
+-(void)xmppUserLogin:(XMPPResultsBlock)result;
+
+/**
+ *  用户注销
+ */
+-(void)xmppUserlogout;
+
+/**
+ *  用户注册
+ */
+-(void)xmppUserRegister:(XMPPResultsBlock)resultBlock;
+
+@end
